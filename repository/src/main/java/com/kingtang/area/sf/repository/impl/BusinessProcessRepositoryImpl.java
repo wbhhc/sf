38881de8f666dd379f6a6a827900520f5674ebda
李雄斌 <wbhhc@163.com>
2020-09-10 17:53:46 +0800

    sql实现，mysql

--- repository/src/main/java/com/kingtang/area/sf/repository/impl/BusinessProcessRepositoryImpl.java
+++ repository/src/main/java/com/kingtang/area/sf/repository/impl/BusinessProcessRepositoryImpl.java
@@ -2,12 +2,28 @@ package com.kingtang.area.sf.repository.impl;
 
 import com.kingtang.area.sf.business.domain.BusinessProcess;
 import com.kingtang.area.sf.repository.BusinessProcessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class BusinessProcessRepositoryImpl implements BusinessProcessRepository {
+
+    String insertSql="INSERT INTO sf_business_process ( APP_ID , BUSINESS_CODE , STATE_CODE ) VALUES (?,?,?)\n";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public void add(BusinessProcess obj) {
+        List<Object> params = new ArrayList<Object>();
+        params.add(obj.getAppId());
+        params.add(obj.getBusinessCode());
+        params.add(obj.getCurrState().getCode());
+        jdbcTemplate.update(insertSql,params.toArray());
 
     }
 }
---
+++ repository/src/test/java/com/kingtang/area/sf/repository/impl/BusinessProcessRepositoryImplTest.java
@@ -0,0 +1,37 @@
+package com.kingtang.area.sf.repository.impl;
+
+import com.kingtang.area.sf.Application;
+import com.kingtang.area.sf.business.domain.BusinessProcess;
+import com.kingtang.area.sf.business.domain.State;
+import com.kingtang.area.sf.repository.BusinessProcessRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
+public class BusinessProcessRepositoryImplTest {
+
+    @Autowired
+    BusinessProcessRepository businessProcessRepository;
+
+    @Test
+    public void add() {
+        BusinessProcess bp=new BusinessProcess();
+        bp.setAppId("ecc-ei");
+        bp.setBusinessCode("123");
+
+        State s=new State();
+        s.setAppId(bp.getAppId());
+        s.setCode("1");
+        s.setName("待流调");
+        bp.setCurrState(s);
+        businessProcessRepository.add(bp);
+    }
+}
